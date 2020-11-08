34a701e3c2236eb3b1039aae1438a034b62691e5
Roycie G <royciegaines@gmail.com>
2020-11-08 13:18:08 -0600

    section 1

--- Orbit-Report-RoycieG/src/app/app.component.ts
+++ Orbit-Report-RoycieG/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Satellite } from './satellite';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Orbit-Report-RoycieG';
+    title = 'Orbit-Report-RoycieG';
+    sourceList: Satellite[];
 }
+
+constructor(){
+  this.sourceList = [
+    new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
+    new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true),
+    new Satellite("Weber Grill", "Space Debris", "1996-03-25", "HIGH", false),
+    new Satellite("GPS 938", "Positioning", "2001-11-01", "HIGH", true),
+    new Satellite("ISS", "Space Station", "1998-11-20", "LOW", true),
+   ];
+}
+  
+
+}
+
+
