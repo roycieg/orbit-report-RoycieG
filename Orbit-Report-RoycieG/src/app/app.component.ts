a65e7f68ebae7a48933d0559d8cb9c8a3565496c
Roycie G <royciegaines@gmail.com>
2020-11-09 19:38:24 -0600

    review

--- Orbit-Report-RoycieG/src/app/app.component.ts
+++ Orbit-Report-RoycieG/src/app/app.component.ts
@@ -21,9 +21,6 @@ export class AppComponent {
             matchingSatellites.push(this.sourceList[i]);
          }
       }
-      // assign this.displayList to be the array of matching satellites
-      // this will cause Angular to re-make the table, but now only containing matches
-      this.displayList = matchingSatellites;
    }
 
     constructor() {
